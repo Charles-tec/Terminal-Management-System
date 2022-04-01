3fac8cb0a7073c1676a42fb7c3ce91c077da79b1
Charles-tec <charlesnyashaphiri02@gmail.com>
2022-04-01 11:26:10 +0200

    exceptions

---
+++ src/main/java/com/terminalmanagementsystem/TerminalmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.terminalmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/terminalmanagementsystem/TerminalmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terminalmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
